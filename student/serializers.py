d7e2bc14624e42425eeae85a62a064c108932d40
bp005 <bijaypdl10@gmail.com>
2026-04-08 09:26:44 +0545

    minor changes in teacher

--- student/serializers.py
+++ student/serializers.py
@@ -1,8 +1,16 @@
 #converts student db objects to json format
 from rest_framework import serializers
 from .models import Student
+from teacher.serializer import TeacherSerializer
+from course.serializer import CourseSerializer
 
 class StudentSerializer(serializers.ModelSerializer):
+    # Overriding the fields to use Nested Serializers
+    # read_only=True ensures we don't break the POST/PUT methods
+    teacher= TeacherSerializer(read_only=True)
+    course= CourseSerializer(read_only=True)
+    
+    
     #This automatically includes all fields of the Student model in the serializer
     class Meta:#setting for the serializer ho yo chai 
         model= Student#model lai link gareko
